2a4326cf1ac63da63b197b40a30f9507a7b14041
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-06-10 12:48:36 +0900

    master

--- Local_MSA/service3/main.py
+++ Local_MSA/service3/main.py
@@ -1,37 +1,64 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import os 
-
-import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date
 
 app = Flask(__name__)
-CORS(app)
-
-conn = mysql.connector.connect(
-    host = 'db',
-    port = 3306,
-    user = os.environ.get('MYSQL_USER'),
-    password = os.environ.get('MYSQL_PASSWORD'),
-    database = os.environ.get('MYSQL_DATABASE')
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET'])
-def select_all():
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
 
-    conn.ping(reconnect=True)
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM COMMENT')
-    sql_result = cur.fetchall()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        posts = Post.query.order_by(Post.due).all()
+        return render_template('index.html', posts=posts, today=date.today())
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
 
-    result = []
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due)
 
-    for item in sql_result :
-        index = item[0]
-        title = item[1]
-        category = item[2]
-        content = item[3]
+        db.session.add(new_post)
+        db.session.commit()
 
-        result.append(dict(index=index, title=title, category=category, content=content))
+        return redirect('/')
 
-    return jsonify(results=result)
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+    return render_template('detail.html', post=post)
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+        db.session.commit()
+        return redirect('/')
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=False)
